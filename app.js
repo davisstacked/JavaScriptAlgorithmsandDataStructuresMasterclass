96b74504213328c4971d9a7e71019c470506eaf4
davisstacked <brettzvi@gmail.com>
2021-02-16 14:48:33 -0800

    space complexity

--- app.js
+++ app.js
@@ -79,3 +79,22 @@ function logAtMost5(n) {
         console.log(i);
     }
 }
+
+// An Example
+function sum(arr) {
+    let total = 0;
+    for (let i = 0; i < arr.length; i++) {
+        total += arr[i];
+    }
+    return total;
+}
+
+// Another example
+
+function double(arr) {
+    let newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        newArr.push(2 * arr[i]);
+    }
+    return newArr;
+}
