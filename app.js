e2af05fd3b037701eade8f887768f70b39a491f4
davisstacked <brettzvi@gmail.com>
2021-02-18 14:59:45 -0800

    notes for frequency counter pattern

--- app.js
+++ app.js
@@ -98,3 +98,133 @@ function double(arr) {
     }
     return newArr;
 }
+
+// 20. Step 4 - Solve or Simplify
+
+// Write a function which takes in a string and returns counts of each character in the string.
+
+function charCount(str) {
+    // make object to return at end
+    let result = {};
+    // loop over string, for each character 
+    for (let i = 0; i < str.length; i++) {
+        let char = str[i].toLowerCase()
+        // if the char is a number/letter AND is a key in object, add one to count
+        if (result[char] > 0) {
+            result[char]++;
+        } else {
+            result[char] = 1;
+        };
+    }
+    // if character is something else (space, period, etc.) don't do anything 
+    // return object at end
+    return result;
+}
+
+console.log(charCount('Hi there!'));
+
+// Write a function called same, which accepts two arrays. The function should return true if every value in the array has it's corresponding value squared in the 2nd array. the frequency of values must be the same. 
+
+// naive solution - O(n^2) - because nested loops.
+function same(arr1, arr2) {
+    if (arr1.length !== arr2.length) {
+        return false;
+    }
+    for (let i = 0; i < arr1.length; i++) {
+        let correctIndex = arr2.indexOf(arr1[i] ** 2)
+        if (correctIndex === -1) {
+            return false;
+        } arr2.splice(correctIndex, 1)
+    }
+    return true;
+}
+
+console.log(same([1, 2, 3, 2], [9, 1, 4, 4]));
+
+// Refactored solution
+// break down the arrays into two objects which can be compared.
+// big O is O(n) so much better than nested arrays.
+function same(arr1, arr2) {
+    if (arr1.length !== arr2.length) {
+        return false;
+    }
+    let frequencyCounter1 = {};
+    let frequencyCounter2 = {};
+    for (let val of arr1) {
+        frequencyCounter1[val] = (frequencyCounter1[val] || 0) + 1
+    }
+    for (let val of arr2) {
+        frequencyCounter2[val] = (frequencyCounter2[val] || 0) + 1
+    }
+    for (let key in frequencyCounter1) {
+        if (!(key ** 2 in frequencyCounter2)) {
+            return false
+        }
+        if (frequencyCounter2[key ** 2] !== frequencyCounter1[key]) {
+            return false
+        } 
+    }
+    return true
+}
+
+// 28. frequency counter: anagram challenge
+// Anagrams 
+// Given two strings, write a function to determine if the second string is an anagram of the first. An anagram is a word, phrase, or name formed by rearranging the letters of another such as cinema, formed from iceman.
+
+// Take two words and see if they contain all of the same letters in a different order
+
+// if the words are the same they are not anagrams 
+// if one word has more letters than the other word they are not anagrams
+
+// my solution to 29. Anagram Challenge solution
+function anagram(str1, str2) {
+    if (str1 === str2) {
+        return false;
+    }
+    if (str1.length !== str2.length) {
+        return false;
+    }
+    let frequencyCounter1 = {};
+    let frequencyCounter2 = {};
+    for (let val of str1) {
+        frequencyCounter1[val] = (frequencyCounter1[val] || 0) + 1
+    };
+    for (let val of str2) {
+        frequencyCounter2[val] = (frequencyCounter2[val] || 0) + 1
+    };
+    for (let key in frequencyCounter1) {
+        if (!key in frequencyCounter2) {
+            return false;
+        }
+    }
+    return true;
+}
+
+console.log(anagram('ello', 'hello'));
+
+// Prof's solution to 29. Anagram Challenge solution
+function validAnagram(first, second) {
+    if (first.length !== second.length) {
+        return false;
+    }
+
+    const lookup = {};
+
+    for (let i = 0; i < first.length; i++) {
+        let letter = first[i];
+        // if letter exists, increment, otherwise set to 1
+        lookup[letter] ? lookup[letter] += 1 : lookup[letter] = 1;
+    }
+
+    for (let i = 0; i < second.length; i++) {
+        let letter = second[i];
+        // can't find letter or letter is zero then it's not an anagram
+        if (!lookup[letter]) {
+            return false;
+        } else {
+            lookup[letter] -= 1;
+        }
+    }
+
+    return true;
+}
