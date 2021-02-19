d82be5bf3c50c9e73a6dd0b6336528cbf9d7f8d6
davisstacked <brettzvi@gmail.com>
2021-02-18 16:19:18 -0800

    section 30 complete

--- app.js
+++ app.js
@@ -228,3 +228,38 @@ function validAnagram(first, second) {
 
     return true;
 }
+
+// 30. MULTIPLE POINTERS PATTERN
+//An example
+// Write a function called sumZero which accepts a SORTED array of integers. The function should find the FIRST pair where the sum is 0. Return an array that includes both values that sum to zero or undefined if a pair does not exist.
+
+// Naive Solution - Not ideal - Time Complexity O(N^2) -- Space Complexity O(1)
+function sumZero(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[i] + arr[j] === 0) {
+                return [arr[i], arr[j]];
+            }
+        }
+    }
+}
+// 30. 
+// sumZero Refactored with MULTIPLE POINTERS PATTERN 
+function sumZero(arr) {
+    let left = 0;
+    let right = arr.length - 1;
+    while (left < right) {
+        let sum = arr[left] + arr[right];
+        if (sum === 0) {
+            return [arr[left], arr[right]];
+        } else if (sum > 0) {
+            right--;
+        } else {
+            left++
+        }
+    }
+}
+
+console.log(sumZero([-4, -3, -2, -1, 0, 1, 2, 3, 10]))
+
+
