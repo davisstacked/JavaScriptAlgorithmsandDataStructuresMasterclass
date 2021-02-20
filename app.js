cbbaac25a991b9565bd3936a9797510baab141e4
davisstacked <brettzvi@gmail.com>
2021-02-19 16:34:24 -0800

    added section 34 DIVIDE AND CONQUER

--- app.js
+++ app.js
@@ -316,3 +316,36 @@ function maxSubarraySum(arr, num) {
     }
     return maxSum;
 }
+
+// 34. Given a sorted array of integers, write a function called search, that accepts a value and returns the index where the value passed to the function is located. if the value is not found, return -1.
+
+// 34. a naive solution - linear search - Time Complexity O(N)
+function search(arr, val) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === val) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+// 34. DIVIDE AND CONQUER - Refactor - Time Complexity - Log(N) - Binary Search!
+
+function search(array, val) {
+    let min = 0;
+    let max = array.length - 1;
+
+    while (min <= max) {
+        let middle = Math.floor((min + max) / 2);
+        let currentElement = array[middle];
+
+        if (array[middle] < val) {
+            min = middle + 1;
+        }
+        else {
+            return middle;
+        }
+    }
+
+    return -1;
+}
