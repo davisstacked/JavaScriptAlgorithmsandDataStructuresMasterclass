40d1025fb5fc361dcaab1650bf4f1e6ef2b07c03
davisstacked <brettzvi@gmail.com>
2021-05-03 13:59:27 -0700

    coding challenge 1: frequency counter - validAnagram

--- Section 5: Problem Solving Patterns/app.js
+++ Section 5: Problem Solving Patterns/app.js
@@ -1,26 +1,56 @@
-function same(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
+// function same(arr1, arr2) {
+//   if (arr1.length !== arr2.length) {
+//     return false;
+//   }
+//   let frequencyCounter1 = {}
+//   let frequencyCounter2 = {}
+//   for (let val of arr1) {
+//     frequencyCounter1[val] = (frequencyCounter1[val] || 0) + 1
+//   }
+//   for (let val of arr2) {
+//     frequencyCounter2[val] = (frequencyCounter2[val] || 0) + 1
+//   }
+//   for (let key in frequencyCounter1) {
+//     if (!(key ** 2 in frequencyCounter2)) {
+//       return false
+//     }
+//     if (frequencyCounter2[key ** 2] !== frequencyCounter1[key]) {
+//       return false
+//     }
+//   }
+//   console.log(frequencyCounter1);
+//   console.log(frequencyCounter2)
+//   return true
+// }
+
+// console.log(same([1, 2, 3, 2], [9, 1, 4, 4]))
+
+// Anagrams
+// Given two strings, write a function to determine if the second string is an anagram of the first. An anagram is a word, phrase, or name formed by rearranging the letters of another, such as "cinema" formed from iceman.
+
+function validAnagram(str1, str2) {
+  if (str1.length !== str2.length) {
     return false;
   }
-  let frequencyCounter1 = {}
-  let frequencyCounter2 = {}
-  for (let val of arr1) {
-    frequencyCounter1[val] = (frequencyCounter1[val] || 0) + 1
-  }
-  for (let val of arr2) {
-    frequencyCounter2[val] = (frequencyCounter2[val] || 0) + 1
-  }
+  let frequencyCounter1 = {};
+  let frequencyCounter2 = {};
+  for (let letter of str1) {
+    frequencyCounter1[letter] = (frequencyCounter1[letter] || 0) + 1;
+  };
+  for (let letter of str2) {
+    frequencyCounter2[letter] = (frequencyCounter2[letter] || 0) + 1;
+  };
   for (let key in frequencyCounter1) {
-    if (!(key ** 2 in frequencyCounter2)) {
-      return false
+    if (!(key in frequencyCounter2)) {
+      return false;
     }
-    if (frequencyCounter2[key ** 2] !== frequencyCounter1[key]) {
-      return false
+    if (frequencyCounter1[key] !== frequencyCounter2[key]) {
+      return false;
     }
   }
   console.log(frequencyCounter1);
   console.log(frequencyCounter2)
-  return true
-}
+  return true;
+};
 
-console.log(same([1, 2, 3, 2], [9, 1, 4, 4]))
+console.log(validAnagram('timetwisttext', 'texttwisttime'));
