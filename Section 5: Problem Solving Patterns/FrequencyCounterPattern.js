a1f779010d8c6fab107b7cd3ba9d891c48ee1434
davisstacked <brettzvi@gmail.com>
2021-05-05 08:34:07 -0700

    solved sameFrequency coding challenge 3

--- Section 5: Problem Solving Patterns/FrequencyCounterPattern.js
+++ Section 5: Problem Solving Patterns/FrequencyCounterPattern.js
@@ -54,3 +54,41 @@ function validAnagram(str1, str2) {
 };
 
 console.log(validAnagram('timetwisttext', 'texttwisttime'));
+
+
+// Coding exercise three
+// Write a function called sameFrequency. Given two positive integers, find out if the two numbers have the same frequency of digits.
+// Your solution MUST have the following complexities: Time: O(N)
+
+// My solution
+
+function sameFrequency(num1, num2) {
+  if (num1.length !== num2.length) {
+    return false;
+  }
+  // converts a number to an iterable string
+  str1 = "" + num1;
+  str2 = "" + num2;
+  let frequencyCounter1 = {};
+  let frequencyCounter2 = {};
+  for (let val of str1) {
+    frequencyCounter1[val] = (frequencyCounter1[val] || 0) + 1;
+  }
+  for (let val of str2) {
+    frequencyCounter2[val] = (frequencyCounter2[val] || 0) + 1;
+  }
+  for (let key in frequencyCounter1) {
+    if (!(key in frequencyCounter2)) {
+      return false;
+    }
+    if (frequencyCounter1[key] !== frequencyCounter2[key]) {
+      return false;
+    }
+  };
+  console.log(frequencyCounter1);
+  console.log(frequencyCounter2);
+  return true;
+};
+
+console.log(sameFrequency(123456123456, 654321654321));
+console.log(sameFrequency(12345671234567, 23456782345678));
