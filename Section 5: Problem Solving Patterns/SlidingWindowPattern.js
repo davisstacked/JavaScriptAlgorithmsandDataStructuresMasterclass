3a0fafb0f0ed59219312c735b20234112bd1fda2
davisstacked <brettzvi@gmail.com>
2021-05-13 12:17:53 -0700

    sliding window pattern

--- Section 5: Problem Solving Patterns/SlidingWindowPattern.js
+++ Section 5: Problem Solving Patterns/SlidingWindowPattern.js
@@ -39,5 +39,91 @@ console.log(maxSubarraySum([], 4)) // null
 // now subtract the first number from temp sum and add the next number
 // now maxSum = 10
 // tempSum = 17
-// compare these two numbers and change the maxSum wo whichever is greater. 
+// compare these two numbers and change the maxSum to whichever is greater. 
 // continue until the entire array has been checked.
+
+// Write a function called minSubArrayLen which accepts two paramaters - an array of positive integers and a positive integer. 
+// This function should return the minimum length of a contiguous subarray of which the sum is greater than or equal to the integer passed into the function. If there isn't one, return 0 instead. 
+
+// My solution
+
+// const minSubArrayLen = (arr, num) => {
+//   if (!num) return 0;
+//   let currentTotal = 0;
+//   // create a number count to represent how many elements are added together to get a total equal to or greater than num
+//   let currentLen = 0;
+//   // create a temporary number count so we can compare elements and find the minimum length
+//   let minLen = 0;
+
+//   for (let i = 0; i < arr.length; i++) {
+//     // if just one number is greater than goal number then return one - we only needed the one number
+//     if (arr[i] >= num) return 1;
+//     // if that one number is less than goal number than
+//     if (arr[i] < num) {
+//       // add one to count that number in how many are needed to equal or beat goal number. 
+//       currentLen++;
+//       // and make the current total equal to the first number in our array. We keep adding numbers on and moving forward in the array until the current total is greater than our number. 
+//       currentTotal += arr[i];
+//       if (currentTotal >= num) {
+//         // once that happens, make the current length we've found equal to our min length. And set current length back to one.
+//         minLen = currentLen;
+//         currentLen = i;
+//       }
+//     }
+//   }
+// };
+
+function minSubArrayLen(nums, sum) {
+  let total = 0;
+  let start = 0;
+  let end = 0;
+  // we initialize to infinity so that our first number will be smaller when comparing in the while loop
+  let minLen = Infinity;
+ 
+  while (start < nums.length) {
+    // if current window doesn't add up to the given sum then add the first number in array to total and then move on to the next number
+		// move the window to right
+    if(total < sum && end < nums.length){
+      total += nums[end];
+			end++;
+    }
+    // if current window adds up to at least the sum given then
+		// we can shrink the window 
+    
+    // when the total of the numbers in the array you've been adding are greater than the target number
+    else if (total >= sum) {
+      // compare infinity to how ever many numbers we needed to equal or exceed the goal number and make minLen whichever is smaller.
+      minLen = Math.min(minLen, end - start);
+      // now we subtract the first number in the array from our total. And move our start number over one in the array.
+			total -= nums[start];
+			start++;
+    } 
+    // current total less than required total but we reach the end, need this or else we'll be in an infinite loop 
+    else {
+      break;
+    }
+  }
+ 
+  return minLen === Infinity ? 0 : minLen;
+}
+
+
+// write a function called findLongestSubstring, which accepts a string and returns the longest substring from all distinct characters.
+
+function findLongestSubstring(str) {
+  let longest = 0;
+  let seen = {};
+  let start = 0;
+ 
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+    if (seen[char]) {
+      start = Math.max(start, seen[char]);
+    }
+    // index - beginning of substring + 1 (to include current in count)
+    longest = Math.max(longest, i - start + 1);
+    // store the index of the next char so as to not double count
+    seen[char] = i + 1;
+  }
+  return longest;
+}
