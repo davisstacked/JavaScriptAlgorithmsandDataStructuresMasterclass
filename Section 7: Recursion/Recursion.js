dfaf5e78a26a216eb60b57d211fda7a8d05a0911
davisstacked <brettzvi@gmail.com>
2021-05-14 11:43:25 -0700

    solved ispalindrome exercise 16

--- Section 7: Recursion/Recursion.js
+++ Section 7: Recursion/Recursion.js
@@ -134,4 +134,25 @@ function reverse(str) {
   return reverse(str.slice(1)) + str[0];
 }
 
-reverse("hellodolly");
+// reverse("hellodolly");
+
+// Write a recursive function called isPalindrome which returns true if the string passed to it is a palindrome (reads the same forward and backward). Otherwise it returns false.
+//  my try. doesnt work.
+// function isPalindrome(str) {
+//   if (str.length === 0) return false;
+//   if (str.length === 1) return true;
+//   let reversedString = isPalindrome(str.slice(1)) + str[0];
+//   console.log(reversedString);
+//   if (reversedString === str) return true;
+//   else return false;
+// };
+
+// console.log(isPalindrome('tacocat'));
+
+//  Recursive solution to isPalindrome
+function isPalindrome(str){
+  if(str.length === 1) return true;
+  if (str.length === 2) return str[0] === str[1];
+  if(str[0] === str.slice(-1)) return isPalindrome(str.slice(1,-1))
+  return false;
+}
