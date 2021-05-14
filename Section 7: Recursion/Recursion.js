495880f45772d95a1548041c1f3b40f479f2e392
davisstacked <brettzvi@gmail.com>
2021-05-13 18:28:37 -0700

    coding exercises up to 15 in recursion section

--- Section 7: Recursion/Recursion.js
+++ Section 7: Recursion/Recursion.js
@@ -87,8 +87,37 @@ function power(base, exponent) {
 // Write a function factorial which accepts a number and returns the factorial of that number.
 // A factorial is the product of an integer and all the integrs below it; e.g., factorial four (4!) is equal to 24, because 4 * 3 * 2 *1 equals 24. factorial(0!) is always 1.
 
-function factorial(num) {
-  if (num < 0) return 0;
-  if (num <=1) return 1;
-  return num * factorial(num - 1);
+// function factorial(num) {
+//   if (num < 0) return 0;
+//   if (num <=1) return 1;
+//   return num * factorial(num - 1);
+// }
+
+// Write a function called productOfArray which takes in an array of numbers and returns the product of them all. 
+function productOfArray(arr) {
+  if (arr.length === 0) return 1;
+  return arr[0] * productOfArray(arr.slice(1));
 }
+
+// write a function called recursiveRange which accepts a number and adds up all the numbers from 0 to the number passed to the function
+
+function recursiveRange(num) {
+  if (num === 0) return 0;
+  return num + recursiveRange(num - 1);
+}
+
+// Write a recursive function called fib which accepts a number and returns the nth number in the Fibonacci sequence. Recall that the Fibonacci sequence is the sequence of whole numbers 1, 1, 2, 3, 5, 8, ... which starts with 1 and 1, and where every number therafter is equal to the sum of the previous two numbers. 
+
+function fib(n) {
+  if (n <= 2) return 1;
+  return fib(n - 1) + fib(n - 2);
+  // for n = 6 this would be 5 + 3
+}
+
+console.log(fib(6));
+
+// Coding exercise 15: reverse
+// Write a function called reverse which accepts a string and returns a new string in reverse.
+function reverse() {
+
+};
