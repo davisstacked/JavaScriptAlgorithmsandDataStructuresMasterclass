d5634ebff76bcee1fb8ec0af276e82c4331749a8
davisstacked <brettzvi@gmail.com>
2021-05-15 12:37:16 -0700

    uppercase first letter

--- Section 7: Recursion/Recursion.js
+++ Section 7: Recursion/Recursion.js
@@ -205,15 +205,15 @@ function flatten(oldArr){
 //   }
 // };
 
-function capitalizeWords (array) {
+function capitalizeFirst (array) {
   if (array.length === 1) {
-    return [array[0].toUpperCase()];
+    return [array[0][0].toUpperCase() + array[0].substr(1)];
   }
-  // creating a new array that doesn't include the last element
-  let res = capitalizeWords(array.slice(0, -1));
-  res.push(array.slice(array.length-1)[0].toUpperCase());
+  // res returns a new array minus the last element. this is only modifying the original array.
+  const res = capitalizeFirst(array.slice(0, -1));
+  // capitalize the first letter of the last word in the array.
+  const string = array.slice(array.length - 1)[0][0].toUpperCase() + array.slice(array.length-1)[0].substr(1);
+  res.push(string);
+  console.log(array.slice(array.length - 1));
   return res;
- 
 }
-
-console.log(capitalizeFirst(['car','taco','banana']))
