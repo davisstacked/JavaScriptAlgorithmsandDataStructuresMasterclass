e988d409855bb77cc9902d548bfd16555b6b01be
davisstacked <brettzvi@gmail.com>
2021-05-17 11:35:35 -0700

    binary search solution

---
+++ Searching Algorithms/Searching.js
@@ -0,0 +1,45 @@
+// Linear Search Pseudocode
+
+// This function accepts an array and a value
+// Loop through the array and check if the current element is equal to the value
+// if it is, return the index at which the element is found
+// if the value is never found, return -1 
+
+function linearSearch(arr, val) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === val) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+// Binary Search Pseudocode 
+// this function accepts a sorted array and a value
+// create a left pointer at the start of the array, and a right (length-1) pointer at the end of the array
+// While the left pointer comes before the right pointer
+
+
+// Create a pointer in the middle (can take the average of the length to find a middle value.)
+// If you find the value you want, return the index. 
+// if the value is too small, move the left pointer up
+// if the value is too large, move the right pointer down
+// if you never find the value, return -1 
+// My attempt
+function binarySearch(arr, val) {
+  let start = 0;
+  let end = arr.length - 1;
+  // middle index
+  let middle = Math.floor((start + end) / 2);
+  while (arr[middle] !== val && start <= end) {
+    if (val < arr[middle]) {
+      end = middle -1;
+    } else {
+      start = middle + 1;
+    }
+    middle = Math.floor((start + end) / 2);
+  };
+  return (arr[middle] === val) ? middle : -1;
+};
+
+console.log(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 7));
