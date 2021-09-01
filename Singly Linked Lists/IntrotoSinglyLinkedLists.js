0d8bd99a09ce713596404d20fa95e712c5c32f52
davisstacked <brettzvi@gmail.com>
2021-09-01 16:34:58 -0700

    intro to singly linked lists

---
+++ Singly Linked Lists/IntrotoSinglyLinkedLists.js
@@ -0,0 +1,20 @@
+// What is a Linked list?
+
+// A data structure that contains a HEAD, TAIL and LENGTH property
+// Linked Lists consist of nodes, and each node has a VALUE and a POINTER to another node or null
+// like a bunch of train cars connected. but you cant find things by index. have to start at the first one then move to the next one and move to the next one. 
+
+// time as humans perceive it is a singly linked list?
+
+
+// comparisons with arrays
+
+// Lists
+// Do not have indexes!
+// Connected via nodes with next pointers
+// Random access is not allowed (cant just ask for 10th item and immediately get it - have to traverse from the beginning)
+
+// Arrays
+// Indexed in order!
+// Insertion and deletion can be expensive
+// Can quickly be accessed at a specific index
