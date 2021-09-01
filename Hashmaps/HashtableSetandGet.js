22b244ab78aed92b26c600f204556a2c000cf87e
davisstacked <brettzvi@gmail.com>
2021-09-01 10:40:00 -0700

    hash table get

--- Hashmaps/HashtableSetandGet.js
+++ Hashmaps/HashtableSetandGet.js
@@ -14,7 +14,11 @@ class HashTable {
     }
     return total;
   }
-
+  
+  // Set
+  // 1. Accepts a key and a value
+  // 2. Hashes the key (figure out where to store it)
+  // 3. Stores the key-value pair in the hash table array via separate chaining
   set(key, value) {
     let index = this._hash(key);
     if (!this.keyMap[index]) {
@@ -22,24 +26,28 @@ class HashTable {
     }
     this.keyMap[index].push([key, value]);
   }
-
+  
+  // Get
+  // 1. Accepts the key
+  // 2. Hashes the key
+  // 3. Retrieves the key-value pair in the hash table
+  // 4. If the key isn't found, returns undefined
   get(key) {
-
+    let index = this._hash(key);
+    if (this.keyMap[index]) {
+      for (let i = 0; i < this.keyMap[index].length; i++) {
+        if (this.keyMap[index][i][0] === key) {
+            return this.keyMap[index][i][1]
+        }
+      }
+    }
+    return undefined;
   }
 }
 
 let ht = new HashTable();
 ht.set("hello world", "goodbye!!")
 
-// Set / Get
 
-// Set
-// 1. Accepts a key and a value
-// 2. Hashes the key (figure out where to store it)
-// 3. Stores the key-value pair in the hash table array via separate chaining
 
-// Get
-// 1. Accepts the key
-// 2. Hashes the key
-// 3. Retrieves the key-value pair in the hash table
-// 4. If the key isn't found, returns undefined
+
