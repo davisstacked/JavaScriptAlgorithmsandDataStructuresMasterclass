734a34a8ab71b95ad42620c1cdc13efdc8330250
davisstacked <brettzvi@gmail.com>
2021-09-01 15:50:33 -0700

    hash table big o notation and recap

---
+++ Hashmaps/BigOandRecap.js
@@ -0,0 +1,14 @@
+// Big O of Hash Tables (average case
+
+// Insert: 0(1)
+// Deletion: 0(1)
+// Access: 0(1)
+// if searching for a value is 0(n) but searching for a key is 0(1)
+
+// Recap
+// Hash tables are collections of key-value pairs
+// Hash tables can find values quickly given a key
+// Hash tables can add new key-values quickly
+// Hash tables store data in a large array, and work by hashing the keys
+// A good hash should be fast, distribute keys uniformly and be deterministic
+// Separate chaining and linear probing are two strategies used to deal with two keys that hash to the same index.
