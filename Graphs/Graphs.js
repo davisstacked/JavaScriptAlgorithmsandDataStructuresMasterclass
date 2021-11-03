6d82f93f126625247a52d38166fe867c12d77235
davisstacked <brettzvi@gmail.com>
2021-11-02 19:18:33 -0700

    graph implementation complete

--- Graphs/Graphs.js
+++ Graphs/Graphs.js
@@ -11,6 +11,19 @@ class Graph {
     this.adjacencyList[vertex1].push(vertex2);
     this.adjacencyList[vertex2].push(vertext1);
   }
+
+  removeEdge(vertex1, vertex2) {
+    this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(v => v !== vertex2);
+    this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(v => v !== vertex1);
+  }
+
+  removeVertex(vertex) {
+    while (this.adjacencyList[vertex].length) {
+      const adjacentVertext = this.adjacencyList[vertex].pop();
+      this.removeEdge(vertex, adjacentVertext)
+    }
+    delete this.adjacencyList[vertex]
+  }
 }
 
 let g = new Graph();
